9578a3ae6342ee2b910976a52d5d9e2ac0795b2f
Natthakit Khamso <natthakit.khamso@gmail.com>
2016-08-16 02:33:05 +0200

    Formated Slack Message

--- app/Notifications/PostCreated.php
+++ app/Notifications/PostCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Models\Post;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\SlackMessage;
 use Illuminate\Notifications\Notification;
@@ -11,21 +12,26 @@ use Illuminate\Notifications\Messages\MailMessage;
 class PostCreated extends Notification implements ShouldQueue
 {
     use Queueable;
+    /**
+     * @var Post
+     */
+    private $post;
 
     /**
      * Create a new notification instance.
      *
-     * @return void
+     * @param Post $post
      */
-    public function __construct()
+    public function __construct(Post $post)
     {
-        //
+        $this->post = $post;
     }
 
     /**
      * Get the notification channels.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
+     *
      * @return array|string
      */
     public function via($notifiable)
@@ -36,33 +42,45 @@ class PostCreated extends Notification implements ShouldQueue
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
+     *
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', 'https://laravel.com')
-                    ->line('Thank you for using our application!');
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', 'https://laravel.com')
+            ->line('Thank you for using our application!');
     }
 
     /**
      * Get the Slack representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
+     *
      * @return SlackMessage
      */
     public function toSlack($notifiable)
     {
+        $url = url("story/{$this->post->created_at->format('Y-m-d')}/{$this->post->permalink}");
+
         return (new SlackMessage)
-            ->content('A new story has been published');
+            ->success()
+            ->content("{$this->post->user->name} has published a new story")
+            ->attachment(function ($attachment) use ($url) {
+                $attachment->title($this->post->title, $url)
+                    ->fields([
+                        'Content' => strip_tags($this->post->short_description),
+                    ]);
+            });
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
+     *
      * @return array
      */
     public function toArray($notifiable)
